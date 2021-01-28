6bc1c4a9e2cf10303e838e2fe84a0ad98f65bbf8
Wannes Salomé <mail@wannessalome.nl>
2021-01-28 21:59:20 +0100

    Remove node plugin

--- node.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['./index.js', 'plugin:node/recommended'],
-  parser: 'babel-eslint',
-  plugins: ['react-hooks'],
-  rules: {
-    'react/jsx-boolean-value': 'error',
-  },
-};
