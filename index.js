ae53fa3893b73709211351f3ec89b694c8688b96
Wannes Salomé <mail@wannessalome.nl>
2021-01-29 15:06:57 +0100

    Add additional rules

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['import'],
   rules: {
+    '@typescript-eslint/consistent-type-imports': 'error',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
