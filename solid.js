2e4eed5fd36ca7fc73b811b018cdd3543423c570
Wannes Salomé <11842931+waspeer@users.noreply.github.com>
2023-10-31 00:08:31 +0100

    fix: wrong extends reference

--- solid.js
+++ solid.js
@@ -3,7 +3,12 @@
 /** @type {import('eslint').Linter.Config} */
 const solidConfig = {
   plugins: ['solid'],
-  extends: ['./typescript.js', 'solid/typescript', 'plugin:jsx-a11y/recommended', 'prettier'],
+  extends: [
+    './typescript.js',
+    'plugin:solid/typescript',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+  ],
   rules: {
     'jsx-a11y/label-has-associated-control': 'off',
   },
